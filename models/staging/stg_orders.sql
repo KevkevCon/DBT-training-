1e4dcae79a954d8eafbccabb65cb9d406e6f954d
kevinbgm1st@gmail.com <kevinbgm1st@gmail.com>
2024-04-11 15:16:27 +0000

    developing stage orders  et error

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,17 +1,16 @@
-select
-    o.orderid,
-    o.orderdate,
-    o.shipdate,
-    o.shipmode,
-    o.ordersellingprice - o.ordercostprice as orderprofit, -- Corrected alias, removed the dot notation
-    o.ordercostprice,
-    o.ordersellingprice,
-    c.customername,
-    c.segment,
-    c.country,
-    p.category,
-    p.productname
-   
-from {{ ref('raw_orders') }} as o -- Ensure 'raw_orders' is correctly named in your dbt project
-left join {{ ref('raw_customer') }} as c on o.customerid = c.customerid
-left join {{ ref('raw_product') }} as p on o.productid = p.productid
+o.oselectorderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.shipmode
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p 
+on o.productid= p.productid
