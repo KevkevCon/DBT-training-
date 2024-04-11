91cdf23794d71d90a874fe0f1f02f3a3df48c763
kevinbgm1st@gmail.com <kevinbgm1st@gmail.com>
2024-04-11 17:21:24 +0000

    developing training stg_orders met errors

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,16 +1,21 @@
-o.oselectorderid,
+select 
+--from raw orders
+o.orderid,
 o.orderdate,
 o.shipdate,
 o.shipmode,
 o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+--from raw customer
 c.customername,
 c.segment,
 c.country,
-p.category,
-p.productname,
-p.shipmode
-from {{ ref('raw_orders') }} as o
-left join {{ ref('raw_customer') }} as c
-o.customerid = c.customerid
-left join {{ ref('raw_product') }} as p 
-on o.productid= p.productid
+--From raw product
+p.category, 
+p.productname
+
+from {{ ref('raw_orders') }} as o 
+left join {{ ref('raw_customer') }} as c on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p on o.productid = p.productid
+
