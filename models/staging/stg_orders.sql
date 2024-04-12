1551c43de6dad203179204abc12cebdf08f2ecc2
Kevin Bonoo <kevinbgm1st@gmail.com>
2024-04-12 11:11:31 +0000

    Changes to stg_orders met error fix en report weggehaald

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,21 +1,22 @@
-select 
---from raw orders
-o.orderid,
-o.orderdate,
-o.shipdate,
-o.shipmode,
-o.ordersellingprice - o.ordercostprice as orderprofit,
-o.ordercostprice,
-o.ordersellingprice,
---from raw customer
-c.customername,
-c.segment,
-c.country,
---From raw product
-p.category, 
-p.productname
-
-from {{ ref('raw_orders') }} as o 
-left join {{ ref('raw_customer') }} as c on o.customerid = c.customerid
-left join {{ ref('raw_product') }} as p on o.productid = p.productid
-
+select
+    -- from raw_orders
+    orderid,
+    orderdate,
+    shipdate,
+    shipmode,
+    o.customer,
+    o.productid,
+    ordersellingprice,
+    ordercostprice,
+    -- from raw_customer
+    customername,
+    segment,
+    country,
+    -- from raw_product
+    category,
+    productname,
+    subcategorie,
+    ordersellingprice - ordercostprice as orderprofit
+from {{ ref("raw_orders") }} as o
+left join {{ ref("raw_customer") }} as c on o.customer = c.customerid
+left join {{ ref("raw_product") }} as p on o.productid = p.productid
