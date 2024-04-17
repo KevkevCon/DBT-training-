08fe560ba4c5d923a8c2dd0b24c27681b069353c
Kevin Bonoo <kevinbgm1st@gmail.com>
2024-04-17 12:07:41 +0000

    fixed macro error

--- macros/Limit_data_in_dev.sql
+++
@@ -1,5 +0,0 @@
-{% macro limit_data_in_dev() %}
-{% of target.name === 'development'%}
-where {{columnname}} >= dateadd('day', -30, current_timestamp)
-{% endif %}
-{% endmacro %}
--- macros/markup.sql
+++ macros/markup.sql
@@ -1,3 +1,3 @@
-{% macro markup(sellingprice, costprice) %}
-({{sellingprice}} - {{costprice}})/{{costprice}}
-{% endmacro %}
+--{% macro markup(sellingprice, costprice) %}
+--({{sellingprice}} - {{costprice}})/{{costprice}}
+--{% endmacro %}
